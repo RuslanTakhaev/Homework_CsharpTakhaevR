b87e952f7484a1c874a3bffc49e8a04c8e12ff2f
Ruslan <tahaev_ruslan@mail.ru>
2023-01-30 22:03:27 +0500

    save6

---
+++ Homework_5/Program.cs
@@ -0,0 +1,97 @@
+﻿// Задача 34: Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу,
+//  которая покажет количество чётных чисел в массиве.
+// [345, 897, 568, 234] -> 2
+
+// void FillArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(100, 999);
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         Console.Write($"{array[i]} ");
+//     Console.WriteLine();
+// }
+// void FindNum(int[] array)
+// {
+//     int count = 0;
+//     for (int i = 0; i < array.Length; i++)
+//     {
+//         if (array[i] % 2 == 0) count = count + 1;
+//     }
+//     Console.WriteLine($"the number of even numbers in the array {count}");
+// }
+
+// int[] array = new int[4];
+// FillArray(array);
+// PrintArray(array);
+// FindNum(array);
+
+// Задача 36: Задайте одномерный массив, заполненный случайными числами. Найдите сумму элементов, стоящих на нечётных позициях.
+// [3, 7, 23, 12] -> 19
+// [-4, -6, 89, 6] -> 0
+
+// void FillArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         array[i] = new Random().Next(-100, 100);
+// }
+
+// void PrintArray(int[] array)
+// {
+//     for (int i = 0; i < array.Length; i++)
+//         Console.Write($"{array[i]} ");
+//     Console.WriteLine();
+// }
+// void FindArray(int[] array)
+// {
+// int sum = 0;
+// for (int i = 0; i < array.Length; i++)
+// {
+//    sum= array[1]+array[3];
+// }
+// Console.WriteLine($"sum of elements in odd positions = {sum}");
+// }
+
+// int[] array = new int[4];
+// FillArray(array);
+// PrintArray(array);
+// FindArray(array);
+
+// Задача 38: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] array = new double[4];
+
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(10, 100);
+}
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        Console.Write($"{array[i]} ");
+    Console.WriteLine();
+}
+void FindArray(double[] array)
+{
+    double sump = 0;
+    double min = array[0];
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+            min = array[i];
+        else if (array[i] > max)
+            max = array[i];
+    }
+    sump = max - min;
+    Console.WriteLine(sump);
+}
+
+FillArray(array);
+PrintArray(array);
+FindArray(array);
